b53bca460c229ebf9aba8022a27f9c27dc79ebb1
22PHOP2D-Munkh-Uils <22phop2d-munkh-uils@Bilguuns-iMac.local>
2023-01-06 17:46:42 +0800

    render

--- my-song/src/components/pages/Lists.js
+++ my-song/src/components/pages/Lists.js
@@ -1,31 +1,24 @@
 import styles from "../styles/Lists.module.css";
 import { useState, useContext, useEffect } from "react";
 import { DataContext } from "../contexts/DataProvider";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 const YourLibrary = () => {
   const { data, setData } = useContext(DataContext);
+  const { id } = useParams();
 
   console.log(data);
 
   return (
-    data && (
       <div className={styles.container}>
         <div className={styles.container2}>
           <div className={styles.lists}>
-            {data &&
-              data.map((item, index) => {
-                return (
-                  <div className={styles.playlist}>
-                    <div key={item.name + index}>{item.name}</div>
-                  </div>
-                );
-              })}
+            <div>Id: {id}</div>
           </div>
         </div>
       </div>
-    )
   );
 };
 export default YourLibrary;
